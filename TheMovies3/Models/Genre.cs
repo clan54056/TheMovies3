76dad5a5fac521b64450be5ab82c2b8e7eb13cda
clan54056 <128791046+clan54056@users.noreply.github.com>
2023-10-11 22:48:22 +0200

    Add project files.

---
+++ TheMovies3/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies3.Models
+{
+    public class Genre
+    {
+        public string GenreName { get; }
+
+        public Genre(string genreName)
+        {
+            GenreName=genreName;
+        }
+    }
+}
---
+++ TheMovies3/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies3.Models
+{
+    public class Movie
+    {
+        public string Title { get; }
+        public int Length { get; }
+
+        public Movie(string title, int length, string genre)
+        {
+            Title = title;
+            Length = length;
+        }
+    }
+}
